ce9b2415f5d1636b280342b99f7dd5b5b5332b9b
aseelhanoona <107139491+aseelhanoona@users.noreply.github.com>
2022-06-14 19:21:27 +0300

    m

---
+++ index.js
@@ -0,0 +1,82 @@
+window.onload = function(){
+
+
+
+
+
+    const inp =document.getElementById("tweet-text");
+    const tweet =document.getElementById("tweettext");
+    let feedtweet = document.getElementById("feedtweet1");
+    let btn = document.getElementById("tweetbtn");
+    
+    
+    document.addEventListener('click',btn.onclick);
+    
+    btn.onclick = function createTweet(e){
+    
+    
+        e.preventDefault();
+    
+      let newtweet = feedtweet.cloneNode(true);
+      newtweet.className ="newtweet";
+      document.getElementById("newtweety").appendChild(newtweet);
+      tweet.innerHTML = inp.value;
+    
+      
+      
+    } 
+    
+    function save(){
+      var newdata= document.getElementById('newtweet');
+      if(localStorage.getItem('data') == null){
+        localStorage.setItem('data','[]')
+    
+      }
+      var olddata = JSON.parse(localStorage.getItem('data'));
+      olddata.push(newdata);
+    
+      localStorage.setItem('data',JSON.stringify(old-data));
+    }
+    
+    
+    
+    var btnre = document.getElementsByClassName('retweetbtn');
+    
+    
+    btnre.onclick = function retweet(){
+      let newtweet = feedtweet.cloneNode(true);
+      document.getElementById("newtweety").appendChild(newtweet);
+    
+    
+    }
+    
+    
+    var btnlove = document.getElementById('love');
+    
+    function lovefun(){
+    
+             if (love.style.color =="red") {
+                 love.style.color = "navy"
+             }
+             else{
+                 love.style.color = "red"
+             }
+    }
+    
+    }
+    
+    
+    
+    
+    
+      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
